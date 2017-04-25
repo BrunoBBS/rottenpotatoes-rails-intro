8b4c65e1a9403ebdf9399d529204f0760832b3bc
Bruno B. Scholl <bruno.bbs3@hotmail.com>
2017-04-25 11:41:42 -0300

    testing Tue Apr 25 11:41:42 -03 2017

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.order('title')
+    @movies = Movie.order(params[:orderby] ? params[:orderby]:'rating')
   end
 
   def new
